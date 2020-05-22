a4abdef5fe36fa04c8cf42002d23725c03f07efc
thedestineddragon <63957370+thedestineddragon@users.noreply.github.com>
2020-05-22 15:11:48 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,29 @@
+function setup() {
+  createCanvas(800,400);
+  
+}
+
+function draw() {
+  background(220);
+ 
+  rect(5,50,50,350);
+  rect(85,50,50,350);
+  ellipse(70,40,50,50);
+  rect(165,50,50,350);
+  ellipse(150,40,50,50);
+  rect(245,50,50,350);
+  ellipse(230,40,50,50);
+  rect(325,50,50,350);
+  rect(405,50,50,350);
+  rect(485,50,50,350);
+  rect(565,50,50,350);
+  rect(645,50,50,350);
+  ellipse(310,40,50,50);
+  ellipse(390,40,50,50);
+  ellipse(470,40,50,50);
+  ellipse(550,40,50,50);
+  ellipse(630,40,50,50);
+
+  drawSprites
+
+}
